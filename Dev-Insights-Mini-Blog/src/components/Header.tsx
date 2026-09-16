c0a9ce580f762a24147f9272117429a8850bb66f
Millowise01 <l.carew@alustudent.com>
2026-09-16 16:19:47 +0300

    add Header component

---
+++ Dev-Insights-Mini-Blog/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <div className="header__logo">Dev Insights</div>
+      <nav>
+        <a href="#" className="header__nav-link">
+          New Post
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
